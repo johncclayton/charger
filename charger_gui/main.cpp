ea64b4c549527c23b21a21784f9060d612813d95
John Clayton <johnc@filewave.com>
2015-09-29 19:20:29 +0200

    Main code integrated into the GUI

--- charger_gui/main.cpp
+++ charger_gui/main.cpp
@@ -1,11 +1,18 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
+
+#include "client_controller.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+        
+    ClientMessagingController controller;
+    controller.init(20001, 2002);
     
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("comms", &controller);
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     
     return app.exec();
