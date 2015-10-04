4d4982b1ea632c4870806cfbe69419d249729382
John Clayton <johnc@filewave.com>
2015-10-05 00:10:30 +0200

    Send message from UI to back end.

--- lib/message_bus.cpp
+++ lib/message_bus.cpp
@@ -4,13 +4,14 @@
 
 using namespace nzmqt;
 
-MessageBus::MessageBus(ZMQSocket* sub, ZMQSocket* msg, QObject *parent) : QObject(parent)
+MessageBus::MessageBus(ZMQSocket* sub, ZMQSocket* msg, QObject *parent) : 
+    QObject(parent), _pub(sub), _msg(msg)
 {
     connect(msg, SIGNAL(messageReceived(QList<QByteArray>)), 
-            this, SLOT(message(QList<QByteArray>)));
+            this, SLOT(messageReceived(QList<QByteArray>)));
     
     connect(sub, SIGNAL(messageReceived(QList<QByteArray>)), 
-            this, SLOT(notification(QList<QByteArray>)));
+            this, SLOT(notificationReceived(QList<QByteArray>)));
     
     qDebug() << "message bus layer is now active.";
 }
@@ -19,22 +20,32 @@ MessageBus::~MessageBus() {
     qDebug() << "message bus layer has now been destroyed.";
 }
 
-void MessageBus::message(QList<QByteArray> msg) {
-    qDebug() << "message received:" << msg;
+void MessageBus::messageReceived(QList<QByteArray> msg) {
+    qDebug() << "response received:" << msg;
 }
 
-void MessageBus::notification(QList<QByteArray> msg) {
+void MessageBus::notificationReceived(QList<QByteArray> msg) {
     QString topic = QString::fromUtf8(msg.at(0));
     
     if(topic.startsWith("/icharger/channel/")) {
         QByteArray data = msg.at(1);
         ChannelStatus channel;
         channel.setFromJson(data);
-        Q_EMIT channelStatusUpdated(channel);
+        Q_EMIT channelStatusChanged(channel);
     } else if(topic.startsWith("/icharger/device")) {
         QByteArray data = msg.at(1);
         DeviceInfo info;
         info.setFromJson(data);
-        Q_EMIT deviceInfoUpdated(info);
+        Q_EMIT deviceInfoChanged(info);
+    } else {
+        Q_EMIT messageResponseReceived(topic, msg);
     }
+}
+
+void MessageBus::asyncRequest(QString responseTopic, QString requestPayload) {
+    QList<QByteArray> msg;
+    msg << QByteArray();
+    msg << responseTopic.toUtf8();
+    msg << requestPayload.toUtf8();
+    _msg->sendMessage(msg);
 }
--- lib/message_bus.h
+++ lib/message_bus.h
@@ -9,20 +9,42 @@ namespace nzmqt {
     class ZMQSocket;
 }
 
+/**
+ * @brief The MessageBus class provides an interface for the UI/Qml to talk to the 
+ * back end services via ZeroMQ and JSON.  The interface is intended to be entirely
+ * async in nature, the primary mechanism by which messages are sent/received is
+ * the request/subscription channels. 
+ * @see asyncRequest
+ */
 class MessageBus : public QObject
 {
     Q_OBJECT
 public:
     explicit MessageBus(nzmqt::ZMQSocket* pub, nzmqt::ZMQSocket* msg, QObject *parent = 0);
     virtual ~MessageBus();
+
+    /**
+     * @brief asyncRequest publishes a request payload to the back end service and expects
+     * the reply to appear at some point later on the provided topic (which can be anything, 
+     * as long as the requestor has subscribed).  The reply will appear via the 
+     * messageResponseReceived() signal.
+     * @param responseTopic - the topic you want the reply to appear on
+     * @param requestPayload - the message payload - hope you know what it should be.
+     */    
+    void asyncRequest(QString responseTopic, QString requestPayload);
     
 signals:
-    void channelStatusUpdated(const ChannelStatus&);
-    void deviceInfoUpdated(const DeviceInfo&);    
+    void channelStatusChanged(const ChannelStatus&);
+    void deviceInfoChanged(const DeviceInfo&);    
+    void messageResponseReceived(QString topic, QList<QByteArray> msg);
     
 public slots:
-    void message(QList<QByteArray> msg);
-    void notification(QList<QByteArray> msg);
+    void messageReceived(QList<QByteArray> msg);
+    void notificationReceived(QList<QByteArray> msg);    
+    
+private:
+    nzmqt::ZMQSocket* _pub;
+    nzmqt::ZMQSocket* _msg;
 };
 
 #endif // MESSAGE_BUS_H
