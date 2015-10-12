f014b2cb5b296fe98d8ee7f78f8c483a171234a9
John Clayton <johnc@filewave.com>
2015-10-12 22:51:19 +0200

    Sep. encoder from byte memory layout.

--- service/icharger_controller.cpp
+++ service/icharger_controller.cpp
@@ -7,58 +7,58 @@
 #include "usb/icharger_data.h"
 #include "json_helper.h"
 
-struct DeviceOnlyJson : public device_only {
-    QVariantMap toVariantMap() const {
+struct DeviceOnlyJson {
+    QVariantMap toVariantMap(device_only& d) const {
         QVariantMap data;
         
-        data[STR_DEVICE_ONLY_ID] = device_id.value;
-        data[STR_DEVICE_ONLY_SERIAL_NUMBER] = QString::fromLatin1((const char*)device_sn, 12).trimmed();
-        data[STR_DEVICE_ONLY_HW_VERSION] = hw_version.value / 1000.0;
-        data[STR_DEVICE_ONLY_SW_VERSION] = sw_version.value / 1000.0;
-        data[STR_DEVICE_ONLY_CH1_STATUS] = ch1_status.value;
-        data[STR_DEVICE_ONLY_CH2_STATUS] = ch2_status.value;
+        data[STR_DEVICE_ONLY_ID] = d.device_id.value;
+        data[STR_DEVICE_ONLY_SERIAL_NUMBER] = QString::fromLatin1((const char*)d.device_sn, 12).trimmed();
+        data[STR_DEVICE_ONLY_HW_VERSION] = d.hw_version.value / 1000.0;
+        data[STR_DEVICE_ONLY_SW_VERSION] = d.sw_version.value / 1000.0;
+        data[STR_DEVICE_ONLY_CH1_STATUS] = d.ch1_status.value;
+        data[STR_DEVICE_ONLY_CH2_STATUS] = d.ch2_status.value;
         
         return data;
     }
 };
 
-struct ChannelStatusJson : public channel_status {
-    QVariantMap toVariantMap(int channel) const {
+struct ChannelStatusJson {
+    QVariantMap toVariantMap(channel_status& c, int channel) const {
         QVariantMap data;
         
         data[STR_CHANNEL_STATUS_CHANNEL_NUM] = channel;
-        data[STR_CHANNEL_STATUS_OUTPUT_POWER] = quint32(output_power.value);
-        data[STR_CHANNEL_STATUS_OUTPUT_CURRENT] = output_current.value;
-        data[STR_CHANNEL_STATUS_OUTPUT_VOLTAGE] = output_voltage.value / 1000.0;
-        data[STR_CHANNEL_STATUS_OUTPUT_CAPACITY] = quint32(output_capacity.value);
+        data[STR_CHANNEL_STATUS_OUTPUT_POWER] = quint32(c.output_power.value);
+        data[STR_CHANNEL_STATUS_OUTPUT_CURRENT] = c.output_current.value;
+        data[STR_CHANNEL_STATUS_OUTPUT_VOLTAGE] = c.output_voltage.value / 1000.0;
+        data[STR_CHANNEL_STATUS_OUTPUT_CAPACITY] = quint32(c.output_capacity.value);
         
-        data[STR_CHANNEL_STATUS_INPUT_VOLTAGE] = input_voltage.value / 1000.0;
+        data[STR_CHANNEL_STATUS_INPUT_VOLTAGE] = c.input_voltage.value / 1000.0;
         
-        data[STR_CHANNEL_STATUS_TEMP_INTERNAL] = temp_internal.value / 10.0;
-        data[STR_CHANNEL_STATUS_TEMP_EXTERNAL] = temp_external.value / 10.0;
+        data[STR_CHANNEL_STATUS_TEMP_INTERNAL] = c.temp_internal.value / 10.0;
+        data[STR_CHANNEL_STATUS_TEMP_EXTERNAL] = c.temp_external.value / 10.0;
         
         QVariantList cells;
         for(int index = 0; index < MAX_CELLS; ++index) {
-            if(cell_voltage[index] == 1024 && cell_resistance[index] == 1024)
+            if(c.cell_voltage[index] == 1024 && c.cell_resistance[index] == 1024)
                 continue;
             
             QVariantMap cell;
             cell[STR_CHANNEL_STATUS_CELL_NUMBER] = index;
-            cell[STR_CHANNEL_STATUS_CELL_VOLTAGE] = cell_voltage[index] / 1000.0;
-            cell[STR_CHANNEL_STATUS_CELL_BALANCE_STATUS] = balance_status[index];
-            cell[STR_CHANNEL_STATUS_CELL_RESISTANCE] = cell_resistance[index];
+            cell[STR_CHANNEL_STATUS_CELL_VOLTAGE] = c.cell_voltage[index] / 1000.0;
+            cell[STR_CHANNEL_STATUS_CELL_BALANCE_STATUS] = c.balance_status[index];
+            cell[STR_CHANNEL_STATUS_CELL_RESISTANCE] = c.cell_resistance[index];
             cells << cell;
         }    
         
         data[STR_CHANNEL_STATUS_CELLS] = cells;
         
-        data[STR_CHANNEL_STATUS_TOTAL_RESISTANCE] = total_resistance;
-        data[STR_CHANNEL_STATUS_LINE_INTERNAL_RESISTANCE] = line_internal_resistance;
-        data[STR_CHANNEL_STATUS_CYCLE_COUNT] = cycle_count;
-        data[STR_CHANNEL_STATUS_CONTROL_STATUS] = control_status;
-        data[STR_CHANNEL_STATUS_RUN_STATUS] = run_status;
-        data[STR_CHANNEL_STATUS_RUN_ERROR] = run_error;
-        data[STR_CHANNEL_STATUS_DIALOG_BOX_ID] = dialog_box_id;
+        data[STR_CHANNEL_STATUS_TOTAL_RESISTANCE] = c.total_resistance;
+        data[STR_CHANNEL_STATUS_LINE_INTERNAL_RESISTANCE] = c.line_internal_resistance;
+        data[STR_CHANNEL_STATUS_CYCLE_COUNT] = c.cycle_count;
+        data[STR_CHANNEL_STATUS_CONTROL_STATUS] = c.control_status;
+        data[STR_CHANNEL_STATUS_RUN_STATUS] = c.run_status;
+        data[STR_CHANNEL_STATUS_RUN_ERROR] = c.run_error;
+        data[STR_CHANNEL_STATUS_DIALOG_BOX_ID] = c.dialog_box_id;
         
         return data;
     }
@@ -83,10 +83,11 @@ void iCharger_DeviceController::handleTimeout() {
     bool changed = false;
     
     // fetch device status and publish on the bus
-    DeviceOnlyJson device;
-    int r = _device->get_device_only(&device);
+    device_only d;
+    int r = _device->get_device_only(&d);
     if(r == 0) {
-        QVariantMap t = device.toVariantMap();
+        DeviceOnlyJson device;
+        QVariantMap t = device.toVariantMap(d);
         t[STR_DEVICE_ONLY_MANUFACTURER] = _device->manufacturer();
         t[STR_DEVICE_ONLY_PRODUCT] = _device->product();
         
@@ -97,11 +98,13 @@ void iCharger_DeviceController::handleTimeout() {
     }
     
     // fetch info for ch1 and ch2
-    ChannelStatusJson channel[2];
+    channel_status c;
     for(int index = 0; index < 2; ++index) {
-        r = _device->get_channel_status(index, &channel[index]);
+        memset(&c, 0, sizeof(c));
+        r = _device->get_channel_status(index, &c);
         if(r == 0) {
-            QVariantMap t = channel[index].toVariantMap(index);
+            ChannelStatusJson channel;
+            QVariantMap t = channel.toVariantMap(c, index);
             if(t != _latest_channel[index]) {
                 _latest_channel[index] = t;
                 changed = true;
