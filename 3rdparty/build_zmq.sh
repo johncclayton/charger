261f7958c7033a5ce5f70b3bc03d61236e9bd455
John Clayton <johnc@filewave.com>
2016-06-07 22:53:43 +0200

    Tweak.

--- 3rdparty/build_zmq.sh
+++ 3rdparty/build_zmq.sh
@@ -6,7 +6,7 @@ F=$T.gz
 B=$PWD/build
 
 if [ ! -f "$F" ]; then
-	wget http://download.zeromq.org/zeromq-4.1.4.tar.gz 
+	wget https://github.com/zeromq/zeromq4-1/releases/download/v4.1.4/zeromq-4.1.4.tar.gz
 fi
 
 if [ -d "$D" ]; then
