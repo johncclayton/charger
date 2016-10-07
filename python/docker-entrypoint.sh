a4cfcad353cfcc4b7901d79b59d0a086a67a2e38
John Clayton <john_clayton@me.com>
2016-10-07 14:39:14 +0000

    mods to test that the flask framework are working

--- python/docker-entrypoint.sh
+++ python/docker-entrypoint.sh
@@ -3,7 +3,6 @@ echo "Running charger service..."
 
 if [ -d "/data/charger" ]; then
 	cd /data/charger 
-	export FLASK_APP=main.py
         export FLASK_DEBUG=1
-        flask run --host=0.0.0.0
+	python -u main.py
 fi
