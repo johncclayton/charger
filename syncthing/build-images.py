9b1842a727e8d12a25cc9029ab7f5abbc5032913
John Clayton <johnc@filewave.com>
2016-10-18 09:13:28 +0200

    minor doc tweaks in builder script

--- syncthing/build-images.py
+++ syncthing/build-images.py
@@ -3,14 +3,16 @@ from docker import Client
 import os, shutil, tarfile, io
 import xml.etree.ElementTree as ET
 
-def print_build_command(name):
-    dir_name = name
-    name.replace('_', '-')
-    print("docker build -t syncthing-%s keys\%s" % (name, dir_name))
+def print_build_command(host_name):
+    dir_name = host_name
+    host_name.replace('_', '-')
+    print("docker build -t syncthing-%s keys\%s" % (host_name, dir_name))
 
 client = Client()
 
-HOSTS = {
+# These are the hosts/identities that will be involved in the dev environment.  Hard coded as its just
+# me at this point and the whole idea only works for a static/non-distributed dev environment anyway.
+HOST_TO_IMAGE_MAP = {
     "rpi3": "hypriot",
     "win-dev": "alpine",
     "mac-dev": "alpine"
@@ -19,10 +21,11 @@ HOSTS = {
 # build/run the syncthing_cli image to get the configuration
 key_container = client.create_container("johncclayton/syncthing-cli", "/generate-identities.sh",
                                         environment={
-                                            "KEY_HOSTS": " ".join(HOSTS.keys())
+                                            "KEY_HOSTS": " ".join(HOST_TO_IMAGE_MAP.keys())
                                         })
-key_response = client.start(container=key_container.get('Id'))
-client.wait(container=key_container.get('Id'))
+key_container_id = key_container.get('Id')
+key_response = client.start(container=key_container_id)
+client.wait(container=key_container_id)
 
 # now copy the files from within the container to the local disk
 key_pathname = os.path.join(os.getcwd(), "keys")
@@ -35,6 +38,9 @@ strm, stat = client.get_archive(key_container, "/keys")
 data = io.BytesIO(strm.read())
 tarobj = tarfile.open(fileobj=data)
 tarobj.extractall(path=os.getcwd())
+
+# blow away the container we just created... otherwise we leave a bunch of them hanging around.
+client.remove_container(container=key_container_id, v=True, force=True)
 
 # find the syncthing config.xml template and populate it with all the known device Ids.  This
 # file is then used within all the syncthing images to ensure that all hosts know about each other.
@@ -59,7 +65,7 @@ for subdir, dirs, files in os.walk(key_pathname):
 
             # create a Dockerfile for syncthing for each device that is being used.  we need to find the right template
             # and populate only the correct key_name value so that the appropriate keys/PEM files can be located.
-            dockerfile_template = env.get_template('Dockerfile_' + HOSTS[the_name] + '.tpl')
+            dockerfile_template = env.get_template('Dockerfile_' + HOST_TO_IMAGE_MAP[the_name] + '.tpl')
             df = open(os.path.join(key_pathname, the_name, 'Dockerfile'), 'w')
             df.write(dockerfile_template.render(key_name=the_name))
             df.close()
